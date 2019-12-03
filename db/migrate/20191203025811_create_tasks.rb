59fb997359fa393d0bd6de87d187305fdb6292c5
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-02 19:58:55 -0700

    added user and task resources

---
+++ db/migrate/20191203025811_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.integer :priority
+      t.datetime :completed_at
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
